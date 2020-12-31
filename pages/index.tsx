695d0ba3e5a6e196394c89aa24d699565cda60c6
Nicolas Hurtado <nhd15c@gmail.com>
2020-12-31 05:20:09 -0400

    Added animations to Homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
-import { ReactElement, useState, KeyboardEvent } from 'react';
+import {
+  ReactElement, useState, KeyboardEvent, useRef, useEffect,
+} from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { useIntl } from 'react-intl';
 import clsx from 'clsx';
@@ -8,10 +10,28 @@ import ServiceText from '../components/service_text/service_text';
 import Button from '../components/button/button';
 import Contact from '../components/contact/contact';
 import classes from './index.module.scss';
+import useOnScreen from '../hooks/onscreen';
 
 const Home = (): ReactElement => {
   const [contactModal, setContactModal] = useState(false);
   const { formatMessage } = useIntl();
+
+  const offset = '0px 0px 25px 0px';
+
+  const image1 = useRef<HTMLImageElement | null>(null);
+  const image2 = useRef<HTMLImageElement | null>(null);
+  const text1 = useRef<HTMLDivElement | null>(null);
+  const text2 = useRef<HTMLDivElement | null>(null);
+
+  const mockupIsOnScreen = [
+    useOnScreen(image1, false, offset),
+    useOnScreen(image2, false, offset),
+  ];
+
+  const textIsOnScreen = [
+    useOnScreen(text1, false, offset),
+    useOnScreen(text2, false, offset),
+  ];
 
   const t = (id: string): string => formatMessage({ id });
   const showModal = (): void => setContactModal(true);
@@ -31,7 +51,17 @@ const Home = (): ReactElement => {
           <Container className="mb-5" fluid>
             <Row className={clsx(classes.about, 'align-items-center')}>
               <Col xs={12} md={6}>
-                <div className={clsx(classes.content, 'm-auto')}>
+                <div
+                  className={
+                    clsx(
+                      classes.content,
+                      classes.toanim,
+                      classes.left,
+                      textIsOnScreen[0] && classes.animate, 'm-auto',
+                    )
+                  }
+                  ref={text1}
+                >
                   <h2>{t('aboutV5h')}</h2>
                   <p>{t('aboutV5p1')}</p>
                   <p>{t('aboutV5p2')}</p>
@@ -39,21 +69,52 @@ const Home = (): ReactElement => {
               </Col>
 
               <Col xs={12} md={6}>
-                <img className="m-auto d-none d-md-block" src="/img/phones_mockup.png" alt="" />
+                <img
+                  className={
+                    clsx(
+                      classes.toanim,
+                      classes.right,
+                      mockupIsOnScreen[0] && classes.animate,
+                      'm-auto d-none d-md-block',
+                    )
+                  }
+                  src="/img/phones_mockup.png"
+                  alt=""
+                  ref={image1}
+                />
               </Col>
             </Row>
 
             <Row className={clsx(classes.about, 'align-items-center pt-4 pt-sm-5 mt-md-5 pt-md-5')}>
               <Col xs={12} md={6}>
                 <img
-                  className={clsx(classes.heroMockups, 'm-auto d-none d-sm-block pb-5 pb-md-0')}
+                  className={
+                    clsx(
+                      classes.toanim,
+                      classes.down,
+                      mockupIsOnScreen[1] && classes.animate,
+                      'm-auto d-none d-sm-block pb-5 pb-md-0',
+                    )
+                  }
                   src="/img/hero_mockups.png"
                   alt=""
+                  ref={image2}
                 />
               </Col>
 
               <Col xs={12} md={6}>
-                <div className={clsx(classes.content, 'm-auto text-right pt-3 pt-md-0')}>
+                <div
+                  className={
+                    clsx(
+                      classes.content,
+                      classes.toanim,
+                      classes.right,
+                      textIsOnScreen[1] && classes.animate,
+                      'm-auto text-right pt-3 pt-md-0',
+                    )
+                  }
+                  ref={text2}
+                >
                   <h2 className="text-align-right">{t('processh')}</h2>
                   <p className="text-align-right">{t('processp')}</p>
                 </div>
