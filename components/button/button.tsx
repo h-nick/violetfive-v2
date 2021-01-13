a98c78bef993ed811d72435aea9f07a1558683ac
Nicolas Hurtado <nhd15c@gmail.com>
2021-01-13 00:42:31 -0400

    Custom buttons can now act as links

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -4,32 +4,67 @@ import clsx from 'clsx';
 import chevronDoubleRight from '@iconify/icons-bi/chevron-double-right';
 import classes from './button.module.scss';
 
-interface FCProps {
+interface FCPropsButton {
   text: string,
-  hoverType?: 'standard' | 'color',
+  hoverType?: 'standard' | 'color' | 'none',
+  link?: undefined,
+  href?: undefined,
   _className?: string,
   _onClick: () => void,
 }
 
-const Button: FC<FCProps> = ({
-  text, hoverType = 'standard', _className, _onClick,
-}) => (
-  <button
-    className={
-      clsx(
-        classes.root, hoverType === 'standard' ? classes.standard : classes.color,
+interface FCPropsLink {
+  text: string,
+  hoverType?: 'standard' | 'color' | 'none',
+  link: true,
+  href: string,
+  _className?: string,
+  _onClick?: undefined,
+}
+
+const Button: FC<FCPropsButton | FCPropsLink> = ({
+  text, hoverType = 'standard', link, href, _className, _onClick,
+}) => {
+  if (link) {
+    return (
+      <a
+        className={clsx(
+          classes.root,
+          hoverType === 'standard' && classes.standard,
+          hoverType === 'color' && classes.color,
+          _className,
+        )}
+        type="button"
+        href={href}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div className={classes.inner}>
+          <span className="bold mr-2">{ text }</span>
+          <Icon icon={chevronDoubleRight} />
+        </div>
+      </a>
+    );
+  }
+
+  return (
+    <button
+      className={clsx(
+        classes.root,
+        hoverType === 'standard' && classes.standard,
+        hoverType === 'color' && classes.color,
         _className,
         'button-reset',
-      )
-    }
-    type="button"
-    onClick={_onClick}
-  >
-    <div className={classes.inner}>
-      <span className="bold mr-2">{ text }</span>
-      <Icon icon={chevronDoubleRight} />
-    </div>
-  </button>
-);
+      )}
+      type="button"
+      onClick={_onClick}
+    >
+      <div className={classes.inner}>
+        <span className="bold mr-2">{ text }</span>
+        <Icon icon={chevronDoubleRight} />
+      </div>
+    </button>
+  );
+};
 
 export default Button;
